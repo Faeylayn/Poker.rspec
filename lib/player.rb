bb11dd70937252f1db0bcd9e33fcea8239117b16
Angela+Matt <appacademy@Apps-Mac-mini-3.local>
2015-01-30 17:42:54 -0500

    adds player class and specs

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,13 @@
 
 class Player
+
+  def self.discard_parse(string)
+    output = string.split(",")
+    output.map do |el|
+      el = Integer(el)
+    end
+  end
+
   attr_accessor :bankroll, :hand
 
   def initialize(bankroll=100)
@@ -11,7 +19,20 @@ class Player
   end
 
   def discard
+    # input = self.class.discard_parse(gets.chomp)
     @hand.discard
   end
+
+  def fold(game)
+    game.fold(self)
+  end
+
+  def bet(game, amount)
+    raise "you don't have enough money" if amount > @bankroll
+    raise 'bet too small' if amount < game.current_bet
+    game.bet(self, amount)
+    @bankroll -= amount
+  end
+
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -21,13 +21,60 @@ describe Player do
       player = Player.new
       hand = double(:hand)
       player.get_hand(hand)
-      expect(hand).to receive(:discard)
+      expect(hand).to receive(:discard)#.with(:input)
 
       player.discard
     end
   end
 
   describe "#discard_parse" do
-    it "should return an array of numbers"
+    it "should return an array of numbers" do
+      expect(Player.discard_parse('0,3')).to eq([0,3])
+    end
+    it "should check for valid input" do
+      expect{Player.discard_parse("mystring")}.to raise_error(ArgumentError)
+    end
   end
+
+
+  describe "#fold" do
+    it "should send a fold message to the game" do
+      game = double(:game)
+      player = Player.new
+      expect(game).to receive(:fold).with(player)
+      player.fold(game)
+
+    end
+
+  end
+
+  describe "#bet" do
+    it "should send a bet message to the game" do
+      game = double(:game, :current_bet => 10)
+      player = Player.new
+      expect(game).to receive(:bet).with(player, 50)
+      player.bet(game, 50)
+    end
+
+    it "should not send a value less than current bet" do
+      game = double(:game, :current_bet => 60)
+      player = Player.new
+      expect { player.bet(game, 50) }.to raise_error("bet too small")
+    end
+
+    it "should deduct from bankroll on successful bet" do
+      game = double(:game, :current_bet => 10)
+      player = Player.new
+      expect(game).to receive(:bet).with(player, 50)
+      player.bet(game, 50)
+      expect(player.bankroll).to eq(50)
+    end
+
+    it "should not bet more than its bankroll" do
+      game = double(:game, :current_bet => 10)
+      player = Player.new
+      expect { player.bet(game, 500) }.to raise_error("you don't have enough money")
+    end
+  end
+
 end
